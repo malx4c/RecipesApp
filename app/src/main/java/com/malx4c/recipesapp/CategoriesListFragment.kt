4c52a4e2ff53cb6ad096f86098a2e986ed5c226a
Aleksandr Volynski <al.volynski@ariscard.net>
2025-06-03 21:18:15 +0300

    RA-04 add fragment

---
+++ app/src/main/java/com/malx4c/recipesapp/CategoriesListFragment.kt
@@ -0,0 +1,24 @@
+package com.malx4c.recipesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.malx4c.recipesapp.databinding.FragmentListCategoriesBinding
+
+class CategoriesListFragment : Fragment() {
+    private var _binding: FragmentListCategoriesBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("FragmentListCategoriesBinding is null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentListCategoriesBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/malx4c/recipesapp/MainActivity.kt
+++ app/src/main/java/com/malx4c/recipesapp/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.malx4c.recipesapp
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.malx4c.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
     private val binding
-        get() = _binding ?: throw IllegalStateException ("ActivityMainBinding is null")
+        get() = _binding ?: throw IllegalStateException("ActivityMainBinding is null")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.mainContainer, CategoriesListFragment())
+            .commit()
     }
 }
