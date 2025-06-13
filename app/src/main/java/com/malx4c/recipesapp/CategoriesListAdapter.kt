5cdc507042efca418369a94ef3d4c0fbc7007a39
Aleksandr Volynski <al.volynski@ariscard.net>
2025-06-13 12:09:14 +0300

    RA-08 fix

--- app/src/main/java/com/malx4c/recipesapp/CategoriesListAdapter.kt
+++ app/src/main/java/com/malx4c/recipesapp/CategoriesListAdapter.kt
@@ -8,37 +8,32 @@ import androidx.recyclerview.widget.RecyclerView
 import com.malx4c.recipesapp.databinding.ItemCategoryBinding
 import com.malx4c.recipesapp.entities.Category
 
-
 class CategoriesListAdapter(private val dataSet: List<Category>) :
     RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
 
     class ViewHolder(private val binding: ItemCategoryBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        private val image = binding.ivCategories
-        private val textTitle = binding.tvTitleCategories
-        private val textDescription = binding.tvDescriptionCategories
-
         fun bind(category: Category) {
-            textTitle.text = category.title
-            textDescription.text = category.description
+            binding.tvDescriptionCategories.text = category.title
+            binding.tvDescriptionCategories.text = category.description
 
             val drawable = try {
                 val inputStream = this.itemView.context.assets.open(category.imageUrl)
                 Drawable.createFromStream(inputStream, null)
             } catch (e: Exception) {
-                Log.e("fileErr",category.imageUrl, e)
+                Log.e("fileErr", category.imageUrl, e)
                 null
             }
 
-            image.setImageDrawable(drawable)
+            binding.ivCategories.setImageDrawable(drawable)
 
         }
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
 
-        val inflater = LayoutInflater.from (viewGroup.context)
+        val inflater = LayoutInflater.from(viewGroup.context)
         val binding = ItemCategoryBinding.inflate(inflater, viewGroup, false)
 
         return ViewHolder(binding)
